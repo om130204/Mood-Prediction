f22bb38deff552e5d2f8fc68825e4514d6eabb88
om130204 <92009714+om130204@users.noreply.github.com>
2023-10-26 08:40:17 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+    # # q1=form.request["q1"].val
+    # # q2=form.request["q2"].val
+    # # q3=form.request["q3"].val
+    # # q4=form.request["q4"].val
+    # # q5=form.request["q5"].val
+    # # q6=form.request["q6"].val
+    # # q7=form.request["q7"].val
+    # # q8=form.request["q8"].val
+    
+    # # new_data = [[q1,q2,q3,q4,q5,q6,q7,q8
+    # # predicted_result = model.predict(new_dat
